d5748437f71ebf9b8a38565c6ba5bd514c79970a
Hudhaifa Shatnawi <hudhaifa.shatnawi@gmail.com>
2018-09-09 07:47:50 -0500

    - Fixed form action link - Fixed typo

---
+++ src/Extension/DOMSearchFormExtention.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace HudhaifaS\DOM\Search;
+
+use SilverStripe\ORM\DataExtension;
+
+/**
+ *
+ * @author Hudhaifa Shatnawi <hudhaifa.shatnawi@gmail.com>
+ * @version 1.0, Sep 7, 2018 - 5:23:18 PM
+ */
+class DOMSearchFormExtention
+        extends DataExtension {
+
+    public function updateExtensibleSearchSearchForm($form) {
+        if ($form) {
+            
+            if(!$this->data()->StartWithListing) {
+                $this->data()->StartWithListing = 1;
+                $this->data()->write();
+            }
+            $form->setFormAction($this->owner->Link());
+
+            // Replace the search title with a placeholder.
+            $search = $form->Fields()->dataFieldByName('Search');
+            $search->setAttribute('placeholder', _t('DOMSearchEngine.SEARCH', 'Search'));
+        }
+    }
+
+}
